44e877dc5b2a04e80b1df4699888668141b4596c
Mateusz Wojewodzic <34684214+Sulphur0@users.noreply.github.com>
2022-07-02 03:02:37 +0200

    FPS FIXED KURWA

--- My project (1)/Assets/Scripts/Movement.cs
+++ My project (1)/Assets/Scripts/Movement.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
     public CharacterController controller;
+    public GameObject player;
+    public Vector3 respawnPosition, respawnOrientation;
+    public Image deathOverlay;
+    public Camera mainCamera;
     private float speed = 12f;
+    private Vector3 cameraPosition;
+    private Vector3 cameraRotation;
     public float walking_speed = 12f;
     public float sprinting_speed = 25f;
     public float gravity = -40f;
@@ -15,45 +22,79 @@ public class Movement : MonoBehaviour
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+    public LayerMask deathMask;
     bool isGrounded;
+    bool shouldDie, isDead;
 
     public float jumpHeight = 3f;
+    private float epsilon = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-
+        deathOverlay.color = new Color(0.0f,0.0f,0.0f,0.0f);
+        isDead = false;
+        float[] distances = new float[32];
+        distances[6] = 3000.0f;
+        mainCamera.layerCullDistances = distances;
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if(isGrounded)
+        shouldDie = Physics.CheckSphere(groundCheck.position, groundDistance, deathMask);
+        if(shouldDie || isDead)
         {
-            velocity.y = -2f;
-        }
-        
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
-        
-        if(Input.GetButton("Sprint"))
-        {
-            speed = sprinting_speed;
+            if(!isDead){
+                epsilon = 0.0f;
+                cameraPosition = mainCamera.transform.position;
+                cameraRotation = (mainCamera.transform.rotation).eulerAngles;
+                isDead = true;
+            }
+            mainCamera.transform.rotation = Quaternion.Euler(Vector3.Lerp(cameraRotation, new Vector3(cameraRotation.x, cameraRotation.y, cameraRotation.z + 90), epsilon));
+            mainCamera.transform.position = Vector3.Lerp(cameraPosition, new Vector3(cameraPosition.x, cameraPosition.y - 1.6f, cameraPosition.z), epsilon);
+            deathOverlay.color = new Color(0.0f,0.0f,0.0f,Mathf.Clamp((epsilon - 0.5f) * 3.0f, 0.0f, 1.0f));
+            if (epsilon < 1.0f)
+            {
+                epsilon += 0.001f;
+            }
+            else
+            {
+                player.transform.position = respawnPosition;
+                player.transform.rotation = Quaternion.Euler(respawnOrientation);
+                shouldDie = false;
+                isDead = false;
+                deathOverlay.color = new Color(0.0f,0.0f,0.0f,0.0f);
+            }
         }
         else
         {
-            speed = walking_speed;
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if(isGrounded)
+            {
+                velocity.y = -2f;
+            }
+            
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            Vector3 move = transform.right * x + transform.forward * z;
+            
+            if(Input.GetButton("Sprint"))
+            {
+                speed = sprinting_speed;
+            }
+            else
+            {
+                speed = walking_speed;
+            }
+            
+            controller.Move(move * speed * Time.deltaTime);
+            if(Input.GetButton("Jump") && isGrounded)
+            {
+                velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
         }
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetButton("Jump") && isGrounded)
-        {
-            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
     }
 }
